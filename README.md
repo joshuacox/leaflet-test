c5b819c626b82ad3b5d29480c477598578f69a01
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-04-10 10:56:47 -0500

    leaflet quickstart

---
+++ README.md
@@ -0,0 +1,6 @@
+LEAFLET-TEST
+==============
+
+This is simply following along with the quickstart
+
+https://leafletjs.com/examples/quick-start/
---
+++ js/leafer.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([30.244314437207148, -97.76567748716134], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
